6178bfbb12f2b7e78fe36299289a2edf6bdf8d9c
OlTrenin <rucapashna@gmail.com>
2024-12-02 17:43:15 +0200

    Изменение логики header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,97 +1,73 @@
-import * as React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import IconButton from '@mui/material/IconButton';
+import React, { useState, useRef } from 'react';
+import { AppBar, Box, Toolbar, IconButton, Typography, Menu, MenuItem } from '@mui/material';
 import MenuIcon from '@mui/icons-material/Menu';
 import AccountCircle from '@mui/icons-material/AccountCircle';
-// import Switch from '@mui/material/Switch';
-// import FormControlLabel from '@mui/material/FormControlLabel';
-// import FormGroup from '@mui/material/FormGroup';
-import MenuItem from '@mui/material/MenuItem';
-import Menu from '@mui/material/Menu';
-
 
 const Header = () => {
-  const [auth, setAuth] = React.useState(true);
-  const [anchorEl, setAnchorEl] = React.useState(null);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const anchorRef = useRef(null);
 
-  // const handleChange = (event) => {
-  //   setAuth(event.target.checked);
-  // };
-
-  const handleMenu = (event) => {
-    setAnchorEl(event.currentTarget);
+  const handleMenuOpen = () => {
+    setIsMenuOpen(true);
   };
 
-  const handleClose = () => {
-    setAnchorEl(null);
+  const handleMenuClose = () => {
+    setIsMenuOpen(false);
   };
-    return (
+
+  return (
       <Box sx={{ flexGrow: 1 }}>
-      {/* <FormGroup>
-        <FormControlLabel
-          control={
-            <Switch
-              checked={auth}
-              onChange={handleChange}
-              aria-label="login switch"
-            />
-          }
-          label={auth ? 'Logout' : 'Login'}
-        />
-      </FormGroup> */}
-      <AppBar position="static">
-        <Toolbar sx={{background:'rgb(229, 229, 229)'}}>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1, fontFamily: 'Rowdies', fontSize:36,fontWeight:400, color:'rgb(255, 255, 255)' }}>
-            Todo Daily
-          </Typography>
-          {auth && (
+        <AppBar position="static">
+          <Toolbar sx={{ background: 'rgb(229, 229, 229)' }}>
+            <IconButton size="large" edge="start" color="inherit" sx={{ mr: 2 }}>
+              <MenuIcon />
+            </IconButton>
+            <Typography
+                variant="h6"
+                component="div"
+                sx={{
+                  flexGrow: 1,
+                  fontFamily: 'Rowdies',
+                  fontSize: 36,
+                  fontWeight: 400,
+                  color: 'rgb(255, 255, 255)',
+                }}
+            >
+              TodoDaily
+            </Typography>
             <div>
               <IconButton
-                size="large"
-                aria-label="account of current user"
-                aria-controls="menu-appbar"
-                aria-haspopup="true"
-                onClick={handleMenu}
-                color="inherit"
+                  size="large"
+                  aria-controls="menu-appbar"
+                  aria-haspopup="true"
+                  onClick={handleMenuOpen}
+                  color="inherit"
+                  ref={anchorRef}
               >
                 <AccountCircle />
               </IconButton>
               <Menu
-                id="menu-appbar"
-                anchorEl={anchorEl}
-                anchorOrigin={{
-                  vertical: 'top',
-                  horizontal: 'right',
-                }}
-                keepMounted
-                transformOrigin={{
-                  vertical: 'top',
-                  horizontal: 'right',
-                }}
-                open={Boolean(anchorEl)}
-                onClose={handleClose}
+                  id="menu-appbar"
+                  anchorEl={anchorRef.current}
+                  anchorOrigin={{
+                    vertical: 'top',
+                    horizontal: 'right',
+                  }}
+                  transformOrigin={{
+                    vertical: 'top',
+                    horizontal: 'right',
+                  }}
+                  open={isMenuOpen}
+                  onClose={handleMenuClose}
               >
-                <MenuItem onClick={handleClose}>Profile</MenuItem>
-                <MenuItem onClick={handleClose}>My account</MenuItem>
+                <MenuItem onClick={handleMenuClose}>Profile</MenuItem>
+                <MenuItem onClick={handleMenuClose}>My account</MenuItem>
               </Menu>
             </div>
-          )}
-        </Toolbar>
-      </AppBar>
-    </Box>
-    );
-}
- 
+          </Toolbar>
+        </AppBar>
+      </Box>
+  );
+};
+
 export default Header;
