4a464f469c9860ebe642301df7b0a3de061bbe48
OlTrenin <rucapashna@gmail.com>
2025-01-03 09:54:47 +0300

    Рефакторинг filteredTasks

---
+++ src/Helpers/FilteredTasks.jsx
@@ -0,0 +1,15 @@
+export const getFilteredTasks = (tasks, day) => {
+    const today = new Date();
+    return tasks.filter((task) => {
+        const taskDate = new Date(task.date);
+        if(day === "Yesterday") {
+            const yesterday = new Date(today);
+            yesterday.setDate(today.getDate() - 1);
+            return taskDate.toDateString() === yesterday.toDateString();
+        }
+        if(day === "Upcoming") {
+            return taskDate > today;
+        }
+        return taskDate.toDateString() === today.toDateString();
+    })
+}
--- src/components/Todo/TodoListView.jsx
+++ src/components/Todo/TodoListView.jsx
@@ -5,6 +5,7 @@ import {TodoTask} from "./Todo";
 import {ButtonAddTask} from "../Button/ButtonAddTask";
 import { MyModal} from "../MyModal/MyModal";
 import {fetchTodos} from "../../service/Api";
+import {getFilteredTasks} from "../../Helpers/FilteredTasks";
 
 const TodoListView = ({day}) => {
     const [tasks, setTasks] = useState([]);
@@ -66,22 +67,6 @@ const TodoListView = ({day}) => {
             ]);
         }
     };
-
-    const getFilteredTasks = (tasks, day) => {
-        const today = new Date();
-        return tasks.filter((task) => {
-            const taskDate = new Date(task.date);
-            if(day === "Yesterday") {
-                const yesterday = new Date(today);
-                yesterday.setDate(today.getDate() - 1);
-                return taskDate.toDateString() === yesterday.toDateString();
-            }
-            if(day === "Upcoming") {
-                return taskDate > today;
-            }
-            return taskDate.toDateString() === today.toDateString();
-        })
-    }
 
     const filteredTasks = getFilteredTasks(tasks,day)
 
