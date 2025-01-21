70cba65795d68d9fa29746eabbe883d554b83a11
OlTrenin <rucapashna@gmail.com>
2025-01-21 13:02:40 +0200

    изменение prettier

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,14 +1,13 @@
 import './styles/style.css'
-import { BrowserRouter as Router } from "react-router-dom";
-import {Sidebar} from '../shared/ui/sidebar/Sidebar'
+import { BrowserRouter as Router } from 'react-router-dom'
+import { Sidebar } from '../shared/ui/sidebar/Sidebar'
 
 function App() {
-  return (
-      <Router>
-          <Sidebar/>
-      </Router>
-      );
+    return (
+        <Router>
+            <Sidebar />
+        </Router>
+    )
 }
 
-export {App};
-
+export { App }
